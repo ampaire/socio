bff453f843983c998f24d949e777d0ba903260f5
ampaire <femayapr@gmail.com>
2021-06-10 16:52:25 +0300

    Basic homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className= "home-btns">
+        <button className= "btn-acc">Sign Up</button>
+        <div className="mid"></div>
+        <button className= "btn-acc">Login</button>
+      </div>
       </header>
     </div>
   );
